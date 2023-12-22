1031ae7c6ca5a388754af7151eb3e8beaac7a167
FatihYigit35 <fatih.yigit.35@gmail.com>
2023-12-23 02:41:27 +0300

    firebase uygulandı

--- app/src/main/java/com/androiddevelopers/freelanceapp/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/androiddevelopers/freelanceapp/viewmodel/LoginViewModel.kt
@@ -1,7 +1,24 @@
 package com.androiddevelopers.freelanceapp.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.androiddevelopers.freelanceapp.repo.FirebaseRepoInterFace
+import com.google.firebase.auth.FirebaseAuth
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class LoginViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+@HiltViewModel
+class LoginViewModel
+@Inject
+constructor(
+    private val firebaseRepo: FirebaseRepoInterFace,
+    private val firebaseAuth: FirebaseAuth,
+) : ViewModel() {
+    fun getUser() = firebaseAuth.currentUser
+    fun signOut() = firebaseAuth.signOut()
+
+    fun login(email: String, password: String) = firebaseRepo.login(email, password)
+
+    fun forgotPassword(email: String) = firebaseRepo.forgotPassword(email)
+
+    fun getUserDataByDocumentId(documentId: String) = firebaseRepo.getUserDataByDocumentId(documentId)
 }
