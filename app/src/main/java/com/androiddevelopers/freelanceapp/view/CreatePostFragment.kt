e41407dd03921a6e8170e20b765ca04bd828333d
gaffar <gaffaryucell@gmail.com>
2023-12-23 13:59:27 +0300

    BottomNavigationActivity, CreatePostFragment, ProfileFragment eklendi.

---
+++ app/src/main/java/com/androiddevelopers/freelanceapp/view/CreatePostFragment.kt
@@ -0,0 +1,31 @@
+package com.androiddevelopers.freelanceapp.view
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.androiddevelopers.freelanceapp.R
+import com.androiddevelopers.freelanceapp.viewmodel.CreatePostViewModel
+
+class CreatePostFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = CreatePostFragment()
+    }
+
+    private lateinit var viewModel: CreatePostViewModel
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_create_post, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(CreatePostViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/androiddevelopers/freelanceapp/viewmodel/CreatePostViewModel.kt
@@ -0,0 +1,7 @@
+package com.androiddevelopers.freelanceapp.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class CreatePostViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
