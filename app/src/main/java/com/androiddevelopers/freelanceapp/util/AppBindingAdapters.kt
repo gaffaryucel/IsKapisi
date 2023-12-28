7b5b03abb981dfce7036b3d97a5b2d6467c02060
FatihYigit35 <fatih.yigit.35@gmail.com>
2023-12-29 02:49:26 +0300

    hata düzeltmesi yapıldı

--- app/src/main/java/com/androiddevelopers/freelanceapp/util/AppBindingAdapters.kt
+++ app/src/main/java/com/androiddevelopers/freelanceapp/util/AppBindingAdapters.kt
@@ -71,7 +71,7 @@ fun passwordCheck(viewLayout: TextInputLayout, viewText: TextInputEditText) {
         override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
 
         override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-            if (s != null && s.length > 6) {
+            if (s != null && s.length > 5) {
                 if (viewLayout.error != null) {
                     viewLayout.error = null
                 }
