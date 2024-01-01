369e10bdfe1e5e4e6b1b86ea3fb7f9d6efd38fe6
FatihYigit35 <fatih.yigit.35@gmail.com>
2024-01-02 00:16:01 +0300

    home fragment databinding ve hilt uygulandı

--- app/src/main/java/com/androiddevelopers/freelanceapp/view/HomeFragment.kt
+++ app/src/main/java/com/androiddevelopers/freelanceapp/view/HomeFragment.kt
@@ -4,17 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.androiddevelopers.freelanceapp.databinding.FragmentHomeBinding
 import com.androiddevelopers.freelanceapp.viewmodel.HomeViewModel
-import com.androiddevelopers.freelanceapp.viewmodel.RegisterViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class HomeFragment : Fragment() {
-
     private lateinit var viewModel: HomeViewModel
-
     private var _binding: FragmentHomeBinding? = null
 
     // This property is only valid between onCreateView and
@@ -26,7 +24,7 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        viewModel = ViewModelProvider(this)[HomeViewModel::class.java]
+        viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
         return root
--- app/src/main/java/com/androiddevelopers/freelanceapp/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/androiddevelopers/freelanceapp/viewmodel/HomeViewModel.kt
@@ -3,8 +3,16 @@ package com.androiddevelopers.freelanceapp.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class HomeViewModel : ViewModel() {
+@HiltViewModel
+class HomeViewModel
+@Inject
+constructor(
+//    private val firebaseRepo: FirebaseRepoInterFace,
+//    private val firebaseAuth: FirebaseAuth,
+) : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is home Fragment"
