0ce1c67a2c9c11b28fb604a9ba5cad81fe9ea5f9
FatihYigit35 <fatih.yigit.35@gmail.com>
2024-01-06 02:18:52 +0300

    firebase den verileri almak için gerekli kodlamalar yapıldı

--- app/src/main/java/com/androiddevelopers/freelanceapp/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/androiddevelopers/freelanceapp/viewmodel/HomeViewModel.kt
@@ -3,19 +3,58 @@ package com.androiddevelopers.freelanceapp.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.androiddevelopers.freelanceapp.model.jobpost.FreelancerJobPost
+import com.androiddevelopers.freelanceapp.repo.FirebaseRepoInterFace
+import com.androiddevelopers.freelanceapp.util.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel
 @Inject
 constructor(
-//    private val firebaseRepo: FirebaseRepoInterFace,
-//    private val firebaseAuth: FirebaseAuth,
+    private val firebaseRepo: FirebaseRepoInterFace,
 ) : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+    private var _firebaseMessage = MutableLiveData<Resource<Boolean>>()
+    val firebaseMessage: LiveData<Resource<Boolean>>
+        get() = _firebaseMessage
+
+    private var _firebaseLiveData = MutableLiveData<ArrayList<FreelancerJobPost>>()
+    val firebaseLiveData: LiveData<ArrayList<FreelancerJobPost>>
+        get() = _firebaseLiveData
+
+    fun getAllFreelanceJobPost() = viewModelScope.launch {
+        _firebaseMessage.value = Resource.loading(true)
+
+        firebaseRepo.getAllFreelancerJobPostFromFirestore()
+            .addOnSuccessListener {
+
+                _firebaseMessage.value = Resource.loading(false)
+
+                it?.let { querySnapshot ->
+                    val list: ArrayList<FreelancerJobPost> = ArrayList()
+
+                    querySnapshot.forEach { queryDocumentSnapshot ->
+                        list.add(
+                            queryDocumentSnapshot.toObject(FreelancerJobPost::class.java)
+                        )
+                    }
+
+                    _firebaseMessage.value = Resource.success(true)
+
+                    _firebaseLiveData.value = list
+                }
+            }.addOnFailureListener {
+                _firebaseMessage.value = Resource.loading(false)
+
+                it.localizedMessage?.let { message ->
+                    Resource.error(message, false)
+                }
+            }
     }
-    val text: LiveData<String> = _text
+
+
 }
