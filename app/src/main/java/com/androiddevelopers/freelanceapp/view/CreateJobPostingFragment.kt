29409625de11ed5a8d50869d68b7c2c7d59d70f8
FatihYigit35 <fatih.yigit.35@gmail.com>
2024-01-12 23:16:08 +0300

    create job post için gerekli kodlamalar

--- app/src/main/java/com/androiddevelopers/freelanceapp/view/CreateJobPostingFragment.kt
+++ app/src/main/java/com/androiddevelopers/freelanceapp/view/CreateJobPostingFragment.kt
@@ -6,14 +6,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.Navigation
 import com.androiddevelopers.freelanceapp.R
 import com.androiddevelopers.freelanceapp.databinding.FragmentCreateJobPostingBinding
+import com.androiddevelopers.freelanceapp.model.jobpost.EmployerJobPost
+import com.androiddevelopers.freelanceapp.util.JobStatus
+import com.androiddevelopers.freelanceapp.util.Status
 import com.androiddevelopers.freelanceapp.viewmodel.CreateJobPostingViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class CreateJobPostingFragment : Fragment() {
+    private lateinit var view: View
     private lateinit var viewModel: CreateJobPostingViewModel
     private var _binding: FragmentCreateJobPostingBinding? = null
     private val binding get() = _binding!!
@@ -26,7 +32,7 @@ class CreateJobPostingFragment : Fragment() {
     ): View {
         viewModel = ViewModelProvider(this)[CreateJobPostingViewModel::class.java]
         _binding = FragmentCreateJobPostingBinding.inflate(inflater, container, false)
-        val view = binding.root
+        view = binding.root
 
         return view
     }
@@ -35,11 +41,85 @@ class CreateJobPostingFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         errorDialog = AlertDialog.Builder(context).create()
         setupDialogs()
+        setProgressBar(false)
+        observeLiveData(viewLifecycleOwner, view)
+
+        with(binding) {
+            createjobPostButton.setOnClickListener {
+                save(
+                    title = titleTextInputEditText.text.toString(),
+                    description = descriptionTextInputEditText.text.toString(),
+                    skillsRequired = skillsRequiredTextInputEditText.text.toString().split(","),
+                    location = locationsRequiredTextInputEditText.text.toString(),
+                    deadline = deadlineRequiredTextInputEditText.text.toString(),
+                    budget = budgetRequiredTextInputEditText.text.toString().toDouble()
+                )
+            }
+        }
+
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun save(
+        postId: String? = "",
+        title: String? = "",
+        description: String? = "",
+        images: List<String>? = listOf(),
+        skillsRequired: List<String>? = listOf(),
+        budget: Double? = 0.0,
+        deadline: String? = "",
+        location: String? = "",
+        datePosted: String? = "",
+        applicants: List<String>? = listOf(),
+        status: JobStatus? = JobStatus.OPEN,
+        additionalDetails: String? = "",
+        completedJobs: Int? = 0,
+        canceledJobs: Int? = 0,
+        viewCount: Int? = 0,
+        isUrgent: Boolean? = false,
+        employerId: String? = ""
+    ) {
+        viewModel.addJobPostingToFirebase(
+            EmployerJobPost(
+                postId = postId,
+                title = title,
+                description = description,
+                images = images,
+                skillsRequired = skillsRequired,
+                budget = budget,
+                deadline = deadline,
+                location = location,
+                datePosted = datePosted,
+                applicants = applicants,
+                status = status,
+                additionalDetails = additionalDetails,
+                completedJobs = completedJobs,
+                canceledJobs = canceledJobs,
+                viewCount = viewCount,
+                isUrgent = isUrgent,
+                employerId = employerId,
+            )
+        )
+    }
+
+    private fun observeLiveData(owner: LifecycleOwner, view: View) {
+        viewModel.firebaseMessage.observe(owner) {
+            when (it.status) {
+                Status.LOADING -> it.data?.let { state -> setProgressBar(state) }
+                Status.SUCCESS -> {
+                    Navigation.findNavController(view).popBackStack()
+                }
+
+                Status.ERROR -> {
+                    errorDialog.setMessage("${context?.getString(R.string.login_dialog_error_message)}\n${it.message}")
+                    errorDialog.show()
+                }
+            }
+        }
     }
 
     private fun setupDialogs() {
@@ -54,5 +134,14 @@ class CreateJobPostingFragment : Fragment() {
             }
         }
     }
+
+    private fun setProgressBar(isVisible: Boolean) {
+        if (isVisible) {
+            binding.createJobPostProgressBar.visibility = View.VISIBLE
+        } else {
+            binding.createJobPostProgressBar.visibility = View.GONE
+        }
+    }
+
 
 }
