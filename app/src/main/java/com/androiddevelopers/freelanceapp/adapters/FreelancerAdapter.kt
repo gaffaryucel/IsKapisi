ad3be5d838d601433907bce7a2940ee74e05a2a6
FatihYigit35 <fatih.yigit.35@gmail.com>
2024-02-02 01:57:00 +0300

    home page listelemeleri için viewpager eklendi

--- app/src/main/java/com/androiddevelopers/freelanceapp/adapters/FreelancerAdapter.kt
+++ app/src/main/java/com/androiddevelopers/freelanceapp/adapters/FreelancerAdapter.kt
@@ -3,11 +3,14 @@ package com.androiddevelopers.freelanceapp.adapters
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.RecyclerView
+import com.androiddevelopers.freelanceapp.R
 import com.androiddevelopers.freelanceapp.databinding.RowFreelancerJobBinding
 import com.androiddevelopers.freelanceapp.model.jobpost.FreelancerJobPost
 import com.androiddevelopers.freelanceapp.util.AppDiffUtil
+import com.androiddevelopers.freelanceapp.util.downloadImage
 
 class FreelancerAdapter(private val listener: (FreelancerJobPost, View) -> Unit) :
     RecyclerView.Adapter<FreelancerAdapter.FreelancerViewHolder>() {
@@ -39,7 +42,33 @@ class FreelancerAdapter(private val listener: (FreelancerJobPost, View) -> Unit)
 
             cardFreelanceButtonDetail.setOnClickListener { v ->
                 freelancerJobPost.postId?.let {
+                    //görüntüleme sayısı arttırma ve navigasyon işlemlerini
+                    //adapter dışında fragment içinde yapıyoruz
                     listener(freelancerJobPost, v)
+                }
+            }
+
+            val images = freelancerJobPost.images
+            if (images?.size == 0) {
+                layoutImageViewsHome.visibility = View.GONE
+                cardImagePlaceHolderHome.visibility = View.VISIBLE
+                downloadImage(
+                    imagePlaceHolderHome,
+                    ContextCompat.getString(root.context, R.drawable.placeholder)
+                )
+            } else {
+                images?.let { list ->
+                    if (list.size == 1) {
+                        layoutImageViewsHome.visibility = View.GONE
+                        cardImagePlaceHolderHome.visibility = View.VISIBLE
+                        downloadImage(imagePlaceHolderHome, list[0])
+                    } else {
+                        layoutImageViewsHome.visibility = View.VISIBLE
+                        cardImagePlaceHolderHome.visibility = View.GONE
+                        val viewPagerAdapter = ViewPagerAdapterForImages(list)
+                        viewPagerHome.adapter = viewPagerAdapter
+                        indicatorHome.setViewPager(viewPagerHome)
+                    }
                 }
             }
         }
