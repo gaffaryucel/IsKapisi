d0754e1aaa3b33805d35eaff1ef92d0bd27cb65e
FatihYigit35 <fatih.yigit.35@gmail.com>
2024-02-06 01:36:16 +0300

    create fragmentler için popup menu eklendi, bağlantıları yapıldı

--- app/src/main/java/com/androiddevelopers/freelanceapp/view/freelancer/HomeFragment.kt
+++ app/src/main/java/com/androiddevelopers/freelanceapp/view/freelancer/HomeFragment.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.PopupMenu
 import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.LifecycleOwner
@@ -31,6 +32,7 @@ class HomeFragment : Fragment() {
     private lateinit var freelancerAdapter: FreelancerAdapter
     private lateinit var listFreelancerJobPost: ArrayList<FreelancerJobPost>
     private lateinit var errorDialog: AlertDialog
+    private lateinit var popupMenu: PopupMenu
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -67,19 +69,22 @@ class HomeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         errorDialog = AlertDialog.Builder(context).create()
+        popupMenu = PopupMenu(requireActivity(), binding.homeAddIcon)
 
         setProgressBar(false)
         setupDialogs()
+        setupPopupMenu(view)
         observeLiveData(viewLifecycleOwner)
 
         with(binding) {
             search(searchView)
 
-            addIcon.setOnClickListener {
-                //TODO: popup ile create sayfası seçimi yaptır
+            homeAddIcon.setOnClickListener {
+                popupMenu.show()
             }
         }
     }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
@@ -127,6 +132,38 @@ class HomeFragment : Fragment() {
                 context.getString(R.string.ok)
             ) { dialog, _ ->
                 dialog.cancel()
+            }
+        }
+    }
+
+    private fun setupPopupMenu(view: View) {
+        popupMenu.inflate(R.menu.add_popup_menu)
+        popupMenu.setOnMenuItemClickListener { item ->
+            when (item.itemId) {
+                R.id.itemCreateFreelancePost -> {
+                    Navigation
+                        .findNavController(view)
+                        .navigate(R.id.action_global_createPostFragment)
+                    true
+                }
+
+                R.id.itemCreateEmployerPost -> {
+                    Navigation
+                        .findNavController(view)
+                        .navigate(R.id.action_global_createJobPostingFragment)
+                    true
+                }
+
+                R.id.itemCreateDiscoverPost -> {
+                    Navigation
+                        .findNavController(view)
+                        .navigate(R.id.action_global_createDiscoverPostFragment)
+                    true
+                }
+
+                else -> {
+                    false
+                }
             }
         }
     }
