fc5afc91ddfe958a8457e3df28680c602a73f760
FatihYigit35 <fatih.yigit.35@gmail.com>
2024-02-11 03:03:06 +0300

    beğen ve kaydet için gerekli metodlar yazıldı, veritabanı bağlantısı sağlandı

--- app/src/main/java/com/androiddevelopers/freelanceapp/adapters/FreelancerAdapter.kt
+++ app/src/main/java/com/androiddevelopers/freelanceapp/adapters/FreelancerAdapter.kt
@@ -12,8 +12,14 @@ import com.androiddevelopers.freelanceapp.model.jobpost.FreelancerJobPost
 import com.androiddevelopers.freelanceapp.util.AppDiffUtil
 import com.androiddevelopers.freelanceapp.util.downloadImage
 
-class FreelancerAdapter(private val listener: (FreelancerJobPost, View) -> Unit) :
+class FreelancerAdapter(private val userId: String) :
     RecyclerView.Adapter<FreelancerAdapter.FreelancerViewHolder>() {
+    lateinit var clickListener: ((FreelancerJobPost, View) -> Unit)
+    lateinit var likedListener: ((String, Boolean, List<String>) -> Unit)
+    lateinit var savedListener: ((String, Boolean, List<String>) -> Unit)
+
+    private var likeCount: Int? = null
+
     private val diffUtil = AppDiffUtil<FreelancerJobPost>()
 
     private val asyncListDiffer = AsyncListDiffer(this, diffUtil)
@@ -22,7 +28,21 @@ class FreelancerAdapter(private val listener: (FreelancerJobPost, View) -> Unit)
         set(value) = asyncListDiffer.submitList(value)
 
     inner class FreelancerViewHolder(val binding: RowFreelancerJobBinding) :
-        RecyclerView.ViewHolder(binding.root)
+        RecyclerView.ViewHolder(binding.root) {
+        fun onClickCard(freelancerJobPost: FreelancerJobPost, v: View) {
+            clickListener.invoke(freelancerJobPost, v)
+        }
+
+        fun isLiked(postId: String, isLiked: Boolean, likes: List<String>) {
+            likedListener.invoke(postId, isLiked, likes)
+            setLikeAndCount(binding, isLiked, likes.size)
+        }
+
+        fun isSavedPost(postId: String, isSavedPost: Boolean, savedUsers: List<String>) {
+            savedListener.invoke(postId, isSavedPost, savedUsers)
+            setSavedPost(binding, isSavedPost)
+        }
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FreelancerViewHolder {
         val binding =
@@ -37,18 +57,81 @@ class FreelancerAdapter(private val listener: (FreelancerJobPost, View) -> Unit)
     override fun onBindViewHolder(holder: FreelancerViewHolder, position: Int) {
         val freelancerJobPost = freelancerList[position]
 
-        with(holder.binding) {
-            freelancer = freelancerJobPost
+        val likes = freelancerJobPost.likes
+        var isLiked = likes?.contains(userId) ?: false
 
-            cardFreelancer.setOnClickListener { v ->
-                freelancerJobPost.postId?.let {
-                    //görüntüleme sayısı arttırma ve navigasyon işlemlerini
-                    //adapter dışında fragment içinde yapıyoruz
-                    listener(freelancerJobPost, v)
+        val savedUsers = freelancerJobPost.savedUsers
+        var isSavedPost = savedUsers?.contains(userId) ?: false
+
+        val postId = freelancerJobPost.postId.toString()
+
+        with(holder) {
+            with(binding) {
+                freelancer = freelancerJobPost
+
+                setLikeAndCount(binding, isLiked, likes?.size)
+                setSavedPost(binding, isSavedPost)
+                setImageView(binding, freelancerJobPost.images)
+
+                cardFreelancer.setOnClickListener { v ->
+                    onClickCard(freelancerJobPost, v)
+                }
+
+                imageViewFavorite.setOnClickListener {
+                    isLiked = !isLiked
+
+                    if (likes.isNullOrEmpty()) {
+                        isLiked(postId, isLiked, listOf())
+                    } else {
+                        isLiked(postId, isLiked, likes)
+                    }
+                }
+
+                imageViewSaved.setOnClickListener {
+                    isSavedPost = !isSavedPost
+                    if (savedUsers.isNullOrEmpty()) {
+                        isSavedPost(postId, isSavedPost, listOf())
+                    } else {
+                        isSavedPost(postId, isSavedPost, savedUsers)
+                    }
                 }
             }
+        }
+    }
 
-            val images = freelancerJobPost.images
+    private fun setLikeAndCount(binding: RowFreelancerJobBinding, isLiked: Boolean, count: Int?) {
+        with(binding) {
+            count?.let {
+                if (it == 0) {
+                    textCountFavorite.text = ""
+                } else {
+                    textCountFavorite.text = count.toString()
+                }
+            } ?: run {
+                textCountFavorite.text = ""
+            }
+
+            if (isLiked) {
+                imageViewFavorite.setImageResource(R.drawable.ic_fill_favorite)
+            } else {
+                imageViewFavorite.setImageResource(R.drawable.ic_favorite)
+            }
+        }
+
+    }
+
+    private fun setSavedPost(binding: RowFreelancerJobBinding, isSavedPost: Boolean) {
+        with(binding) {
+            if (isSavedPost) {
+                imageViewSaved.setImageResource(R.drawable.baseline_bookmark_24)
+            } else {
+                imageViewSaved.setImageResource(R.drawable.baseline_bookmark_border_24)
+            }
+        }
+    }
+
+    private fun setImageView(binding: RowFreelancerJobBinding, images: List<String>?) {
+        with(binding) {
             if (images?.size == 0) {
                 layoutImageViewsHome.visibility = View.GONE
                 cardImagePlaceHolderHome.visibility = View.VISIBLE
--- app/src/main/java/com/androiddevelopers/freelanceapp/viewmodel/freelancer/HomeViewModel.kt
+++ app/src/main/java/com/androiddevelopers/freelanceapp/viewmodel/freelancer/HomeViewModel.kt
@@ -8,8 +8,6 @@ import com.androiddevelopers.freelanceapp.model.jobpost.FreelancerJobPost
 import com.androiddevelopers.freelanceapp.repo.FirebaseRepoInterFace
 import com.androiddevelopers.freelanceapp.util.JobStatus
 import com.androiddevelopers.freelanceapp.util.Resource
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -18,8 +16,7 @@ import javax.inject.Inject
 class HomeViewModel
 @Inject
 constructor(
-    private val firebaseRepo: FirebaseRepoInterFace,
-    firebaseAuth: FirebaseAuth
+    private val firebaseRepo: FirebaseRepoInterFace
 ) : ViewModel() {
 
     private var _firebaseMessage = MutableLiveData<Resource<Boolean>>()
@@ -30,13 +27,9 @@ constructor(
     val firebaseLiveData: LiveData<List<FreelancerJobPost>>
         get() = _firebaseLiveData
 
-    private var _liveDataFirebaseUser = MutableLiveData<FirebaseUser>()
-    val liveDataFirebaseUser: LiveData<FirebaseUser>
-        get() = _liveDataFirebaseUser
 
     init {
         getAllFreelanceJobPost()
-        _liveDataFirebaseUser.value = firebaseAuth.currentUser
     }
 
     fun getAllFreelanceJobPost() = viewModelScope.launch {
@@ -71,12 +64,71 @@ constructor(
     fun updateViewCountFreelancerJobPostWithDocumentById(
         postId: String,
         newCount: MutableSet<String>
-    ) {
+    ) = viewModelScope.launch {
         val list = arrayListOf<String>()
         list.addAll(newCount)
 
         _firebaseMessage.value = Resource.loading(true)
         firebaseRepo.updateViewCountFreelancerJobPostWithDocumentById(postId, list)
+            .addOnCompleteListener {
+                _firebaseMessage.value = Resource.loading(false)
+                if (it.isSuccessful) {
+                    _firebaseMessage.value = Resource.success(true)
+                } else {
+                    _firebaseMessage.value = Resource.loading(false)
+                    it.exception?.localizedMessage?.let { message ->
+                        _firebaseMessage.value = Resource.error(message, false)
+                    }
+                }
+            }
+    }
+
+    fun updateLikeFreelancerJobPostFromFirestore(
+        userId: String,
+        postId: String,
+        isLiked: Boolean,
+        likes: List<String>
+    ) = viewModelScope.launch {
+        val list = mutableSetOf<String>()
+        list.addAll(likes)
+
+        if (isLiked) {
+            list.add(userId)
+        } else {
+            list.remove(userId)
+        }
+
+        firebaseRepo.updateLikeFreelancerJobPostFromFirestore(postId, list.toList())
+            .addOnCompleteListener {
+                _firebaseMessage.value = Resource.loading(false)
+                if (it.isSuccessful) {
+                    getAllFreelanceJobPost()
+                    _firebaseMessage.value = Resource.success(true)
+                } else {
+                    _firebaseMessage.value = Resource.loading(false)
+                    it.exception?.localizedMessage?.let { message ->
+                        _firebaseMessage.value = Resource.error(message, false)
+                    }
+                }
+            }
+    }
+
+    fun updateSavedUsersFreelancerJobPostFromFirestore(
+        userId: String,
+        postId: String,
+        isSavedPost: Boolean,
+        savedUsers: List<String>
+    ) = viewModelScope.launch {
+        val list = mutableSetOf<String>()
+        list.addAll(savedUsers)
+
+        if (isSavedPost) {
+            list.add(userId)
+        } else {
+            list.remove(userId)
+        }
+
+        firebaseRepo.updateSavedUsersFreelancerJobPostFromFirestore(postId, list.toList())
             .addOnCompleteListener {
                 _firebaseMessage.value = Resource.loading(false)
                 if (it.isSuccessful) {
