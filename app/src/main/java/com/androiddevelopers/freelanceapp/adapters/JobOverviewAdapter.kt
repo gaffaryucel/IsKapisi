e4f5c4189c5ab008a9ee9a90e1f17e31c21d2a2b
FatihYigit35 <fatih.yigit.35@gmail.com>
2024-02-17 23:10:50 +0300

    Yeni employer detay safyası için adapter eklendi

---
+++ app/src/main/java/com/androiddevelopers/freelanceapp/adapters/JobOverviewAdapter.kt
@@ -0,0 +1,49 @@
+package com.androiddevelopers.freelanceapp.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.androiddevelopers.freelanceapp.databinding.CardJobOverviewBinding
+
+class JobOverviewAdapter : RecyclerView.Adapter<JobOverviewAdapter.ViewHolder>() {
+
+    private val diffUtil = object : DiffUtil.ItemCallback<String>() {
+        override fun areItemsTheSame(
+            oldItem: String,
+            newItem: String
+        ): Boolean {
+            return oldItem.length == newItem.length
+        }
+
+        override fun areContentsTheSame(
+            oldItem: String,
+            newItem: String
+        ): Boolean {
+            return oldItem == newItem
+        }
+    }
+    private val asyncListDiffer = AsyncListDiffer(this, diffUtil)
+
+    var jobOverviewList: List<String>
+        get() = asyncListDiffer.currentList
+        set(value) = asyncListDiffer.submitList(value)
+
+    inner class ViewHolder(val binding: CardJobOverviewBinding) :
+        RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding =
+            CardJobOverviewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return jobOverviewList.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.binding.textJobOverviewCard.text = jobOverviewList[position]
+    }
+}
---
+++ app/src/main/java/com/androiddevelopers/freelanceapp/adapters/TextListAdapterForJobDetail.kt
@@ -0,0 +1,54 @@
+package com.androiddevelopers.freelanceapp.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.androiddevelopers.freelanceapp.databinding.RowTextListForJobDetailBinding
+
+class TextListAdapterForJobDetail : RecyclerView.Adapter<TextListAdapterForJobDetail.ViewHolder>() {
+
+    private val diffUtil = object : DiffUtil.ItemCallback<String>() {
+        override fun areItemsTheSame(
+            oldItem: String,
+            newItem: String
+        ): Boolean {
+            return oldItem.length == newItem.length
+        }
+
+        override fun areContentsTheSame(
+            oldItem: String,
+            newItem: String
+        ): Boolean {
+            return oldItem == newItem
+        }
+    }
+    private val asyncListDiffer = AsyncListDiffer(this, diffUtil)
+
+    var textList: List<String>
+        get() = asyncListDiffer.currentList
+        set(value) = asyncListDiffer.submitList(value)
+
+    inner class ViewHolder(val binding: RowTextListForJobDetailBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = RowTextListForJobDetailBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return ViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return textList.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.binding.textViewForJobDetailList.text = textList[position]
+    }
+}
