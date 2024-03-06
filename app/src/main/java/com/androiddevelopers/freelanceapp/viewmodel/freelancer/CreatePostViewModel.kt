cf39438558875a039010c66ec6ef55b1fac67469
FatihYigit35 <fatih.yigit.35@gmail.com>
2024-03-07 00:41:53 +0300

    create sayfası düzenleme

--- app/src/main/java/com/androiddevelopers/freelanceapp/viewmodel/freelancer/CreatePostViewModel.kt
+++ app/src/main/java/com/androiddevelopers/freelanceapp/viewmodel/freelancer/CreatePostViewModel.kt
@@ -1,31 +1,128 @@
 package com.androiddevelopers.freelanceapp.viewmodel.freelancer
 
+import android.net.Uri
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.androiddevelopers.freelanceapp.model.jobpost.EmployerJobPost
+import com.androiddevelopers.freelanceapp.model.jobpost.FreelancerJobPost
 import com.androiddevelopers.freelanceapp.repo.FirebaseRepoInterFace
 import com.androiddevelopers.freelanceapp.util.Resource
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.storage.FirebaseStorage
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import java.util.*
 import javax.inject.Inject
 
 @HiltViewModel
 class CreatePostViewModel @Inject constructor(
-    private val repo: FirebaseRepoInterFace,
+    private val firebaseRepo: FirebaseRepoInterFace,
     private val storage: FirebaseStorage,
-    private val auth: FirebaseAuth
+    private val firebaseAuth: FirebaseAuth
 ) : ViewModel() {
 
     private val storageReference = storage.reference
     private val _uploadPhotoMessage = MutableLiveData<Resource<String>>()
     val uploadPhotoMessage: LiveData<Resource<String>> = _uploadPhotoMessage
 
-    private val currentUserId = auth.currentUser?.uid.toString()
+    private val currentUserId = firebaseAuth.currentUser?.uid.toString()
 
     private var _insertPostMessage = MutableLiveData<Resource<Boolean>>()
     val insertPostMessage: LiveData<Resource<Boolean>>
         get() = _insertPostMessage
+
+    private var _firebaseMessage = MutableLiveData<Resource<Boolean>>()
+    val firebaseMessage: LiveData<Resource<Boolean>>
+        get() = _firebaseMessage
+
+    private var _skills = MutableLiveData<List<String>>()
+    val skills: LiveData<List<String>>
+        get() = _skills
+
+    private var _imageUriList = MutableLiveData<List<Uri>>()
+    val imageUriList: LiveData<List<Uri>>
+        get() = _imageUriList
+
+    private var _imageSize = MutableLiveData<Int>()
+    val imageSize: LiveData<Int>
+        get() = _imageSize
+
+    private var _firebaseLiveData = MutableLiveData<EmployerJobPost>()
+    val firebaseLiveData: LiveData<EmployerJobPost>
+        get() = _firebaseLiveData
+
+    fun addImageAndJobPostToFirebase(
+        newUriList: MutableList<Uri>,
+        post: FreelancerJobPost,
+        downloadUriList: MutableList<String> = mutableListOf()
+    ) {
+        val uId = firebaseAuth.currentUser?.uid.toString()
+        if (newUriList.size > 0) {
+            val uri = newUriList[0]
+            if (uri.toString().contains("firebasestorage")) {
+                newUriList.removeAt(0)
+                downloadUriList.add(uri.toString())
+                addImageAndJobPostToFirebase(newUriList, post, downloadUriList)
+            } else {
+                _firebaseMessage.value = Resource.loading(true)
+                uri.lastPathSegment?.let { file ->
+                    firebaseRepo.addImageToStorageForJobPosting(uri, uId, post.postId!!, file)
+                        .addOnSuccessListener { task ->
+                            task.storage.downloadUrl.addOnSuccessListener {
+                                newUriList.removeAt(0)
+                                downloadUriList.add(it.toString())
+                                addImageAndJobPostToFirebase(
+                                    newUriList, post, downloadUriList
+                                )
+                            }.addOnFailureListener {
+                                _firebaseMessage.value = it.localizedMessage?.let { message ->
+                                    _firebaseMessage.value = Resource.loading(false)
+                                    Resource.error(message, false)
+                                }
+                            }
+                        }.addOnFailureListener {
+                            _firebaseMessage.value = it.localizedMessage?.let { message ->
+                                _firebaseMessage.value = Resource.loading(false)
+                                Resource.error(message, false)
+                            }
+                        }
+                }
+            }
+        } else {
+            if (post.postId == null) {
+                post.postId = UUID.randomUUID().toString()
+            }
+            post.images = downloadUriList
+            post.freelancerId = uId
+            addJobPostingToFirebase(post)
+        }
+    }
+
+    private fun addJobPostingToFirebase(post: FreelancerJobPost) = viewModelScope.launch {
+        _firebaseMessage.value = Resource.loading(true)
+        firebaseRepo.addFreelancerJobPostToFirestore(post).addOnCompleteListener { task ->
+            _firebaseMessage.value = Resource.loading(false)
+            if (task.isSuccessful) {
+                _firebaseMessage.value = Resource.success(true)
+            } else {
+                _firebaseMessage.value = task.exception?.localizedMessage?.let { message ->
+                    Resource.error(message, false)
+                }
+            }
+        }
+        //updateUserData(jobPost)
+    }
+
+    fun setImageUriList(newList: List<Uri>) = viewModelScope.launch {
+        _imageUriList.value = newList
+        _imageSize.value = newList.size
+    }
+
+    fun setSkills(newSkills: List<String>) {
+        _skills.value = newSkills
+    }
 
 //    fun uploadPostPicture(postModel: FreelancerJobPost, r: ByteArray) = viewModelScope.launch {
 //        _uploadPhotoMessage.value = Resource.loading("loading")
